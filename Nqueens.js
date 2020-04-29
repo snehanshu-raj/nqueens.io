68a068320ac1b581858da8015e8b61fbe22d8fd8
srjsnehanshu <57353992+srjsnehanshu@users.noreply.github.com>
2020-04-30 04:55:52 +0530

    nqueens game

--- Nqueens.js
+++ Nqueens.js
@@ -1,4 +1,4 @@
-var count = 0;
+
 
 var old_table = document.createElement("table");
 for (var i = 1; i < 9; i++) {
@@ -7,23 +7,43 @@ for (var i = 1; i < 9; i++) {
         var td = document.createElement('td');
         if (i%2 == j%2) {
             td.className = "white";
-            td.id = count;
         } else {
             td.className = "black";
-            td.id = count;
         }
         tr.appendChild(td);
-        count++;
     }
     old_table.appendChild(tr);
 }
 document.body.appendChild(old_table);
 
 function addtoev() {
-    var set = document.getElementsByTagName("button");
+
+  var set = document.getElementsByTagName("button");
   for (var s = 0; s < set.length; s++) {
     set[s].addEventListener("click", function() {
- 	  count = 0;
+
+        
+        var messagedisplay = document.querySelector("#message");
+        messagedisplay.textContent = "";
+
+        var size = this.id;
+        //console.log(size);
+        var check_solution_button = document.querySelector("#solution");
+        if(check_solution_button){
+            check_solution_button.parentNode.removeChild(check_solution_button);
+
+             var solution_button = document.createElement("button");
+             solution_button.id = "solution";
+             solution_button.textContent = "Check Solution";
+             document.body.appendChild(solution_button);
+        }
+        else{
+            var solution_button = document.createElement("button");
+            solution_button.id = "solution";
+            solution_button.textContent = "Check Solution";
+            document.body.appendChild(solution_button);
+        }
+
         var new_table = document.createElement("table");
 		for (var i = 1; i <= this.id; i++) {
     		var tr = document.createElement('tr');
@@ -31,18 +51,19 @@ function addtoev() {
         	var td = document.createElement('td');
         		if (i%2 == j%2) {
             	td.className = "white";
-            	td.id = count;
+            	td.id = 10*i+j;
         		} else {
             	td.className = "black";
-            	td.id = count;
+            	td.id = 10*i+j;
         	}
         tr.appendChild(td);
-        count++;
     }
     new_table.appendChild(tr);
 }
     old_table.parentNode.replaceChild(new_table, old_table);
     old_table = new_table;
+
+
     var select = document.getElementsByTagName("td");
     for(var i = 0; i < select.length; i++)
     {
@@ -63,12 +84,80 @@ function addtoev() {
                 this.classList.add("black_queen");
             }           
         });
-}
+    }
+    
+    solution_button.addEventListener("click", function(){
+        //alert("clicked");
+        var left_diagonal = [2*size + 1];
+        var right_diagonal = [2*size + 1];
+        var coloumn = [2*size+1];
+        var row = [2*size + 1];
+
+        for(var i = 1; i <= 2*size + 1; i++)
+        {
+            left_diagonal[i] = 0;
+            right_diagonal[i] = 0;
+            coloumn[i] = 0;
+            row[i] = 0;
+        }
+
+        var flag = 1;
+
+        for(var i = 0; i < select.length; i++)
+        {
+            if(select[i].classList.contains("black_queen") || select[i].classList.contains("white_queen")){
+                var n = select[i].id;
+                console.log(n);
+                console.log(Math.floor(n/10));
+                console.log(n%10);
+                if(coloumn[Math.floor(n/10)] == 1)
+                {
+                    messagedisplay.textContent = "WRONG";
+                    flag = 0;
+                    break;
+                }
+                else
+                if(row[(n%10)] == 1)
+                {
+                    messagedisplay.textContent = "WRONG";
+                    flag = 0;
+                    break;
+                }
+                else
+                if(right_diagonal[(Math.floor(n/10)) + (n%10)] == 1)
+                {
+                    messagedisplay.textContent = "WRONG";
+                    flag = 0;
+                    break;
+                }
+                else
+                if(left_diagonal[(Math.floor(n/10)) - (n%10) + size - 1] == 1)
+                {
+                    messagedisplay.textContent = "WRONG";
+                    flag = 0;
+                    break;
+                }
+                else
+                {
+                    coloumn[Math.floor(n/10)] = 1;
+                    row[(n%10)] = 1;
+                    left_diagonal[(Math.floor(n/10)) - (n%10) + size - 1] = 1;
+                    right_diagonal[(Math.floor(n/10)) + (n%10)] = 1;
+                }
+            }
+        }
+        if(flag)
+            messagedisplay.textContent = "CORRECT";       
+        
+
+    });
+
 
 	});
-  }
+  }  
 }
 
+ 
 window.addEventListener("load",function() {
   addtoev();
 });
