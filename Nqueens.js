2910a22fd388b4e2a54144242d04f03383057afb
srjsnehanshu <57353992+srjsnehanshu@users.noreply.github.com>
2020-05-26 15:54:46 +0530

    Final Project Of N Queens Game

--- Nqueens.js
+++ Nqueens.js
@@ -21,10 +21,16 @@ function addtoev() {
 
   var set = document.getElementsByTagName("button");
   for (var s = 0; s < set.length; s++) {
-    set[s].addEventListener("click", function() {
+        set[s].addEventListener("click", function() {
 
         document.body.classList.add("bodyafter");
 
+
+        var intro = document.querySelector("#introduction")
+        if(intro)
+        {
+            intro.parentNode.removeChild(intro);
+        }
 
         var loading_remove = document.querySelector("#loading");
         loading_remove.classList.remove("lds-grid");
@@ -52,6 +58,23 @@ function addtoev() {
             document.body.appendChild(solution_button);
         }
 
+        var check_hint_button = document.querySelector("#hint");
+        if(check_hint_button)
+        {
+            check_hint_button.parentNode.removeChild(check_hint_button);
+
+            var hint_button = document.createElement("button");
+            hint_button.id = "hint";
+            hint_button.textContent = "?";
+            document.body.appendChild(hint_button);
+        }
+        else
+        {
+            var hint_button = document.createElement("button");
+            hint_button.id = "hint";
+            hint_button.textContent = "?";
+            document.body.appendChild(hint_button);   
+        }
 
 
         var new_table = document.createElement("table");
@@ -97,6 +120,34 @@ function addtoev() {
         });
     }
     
+    var all = Math.pow(2, size) - 1;
+    var hint_answer = 0;
+
+    function total_solutions(ld, col, rd)
+    {
+        if(all === col)
+        {
+            hint_answer++;
+        }
+        var position = ~(ld | col | rd) & all;
+
+        while(position)
+        {
+            var bit = position & (-position);
+            position -= bit;
+            total_solutions((ld | bit) << 1, col | bit, (rd | bit) >> 1);
+        }
+        return 0;
+    }
+
+    hint_button.addEventListener("click", function(){
+        var all = Math.pow(2, size) - 1;
+        var count = 0;
+
+        total_solutions(0, 0, 0);
+        alert("There are " + hint_answer + " number of solutions!");
+    });
+
     solution_button.addEventListener("click", function(){
         
         var div;
@@ -215,7 +266,6 @@ function addtoev() {
             messagedisplay.textContent = " Correct ";             
         }
     });
-
 
 	});
   }  
